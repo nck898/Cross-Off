9d51a50ec1bf9615ae42b05d51c9750c073c9a56
Nick Kim <nck898@gmail.com>
2017-09-18 14:06:12 -0400

    updated CSS

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,10 @@
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'practice headspins'}];
+
+
 module.exports = function(app){
 
   app.get('/todo', function(req, res) {
-    res.render('todo');
+    res.render('todo', {todos: data});
   });
 
   app.post('/todo', function(req, res){
